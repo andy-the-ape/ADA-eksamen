d8b9cdb76c89d1684be35927722b94c131d738e4
Andreas Witten Honoré <honore.andreas@gmail.com>
2024-12-20 20:22:47 +0100

    Update Opgave6.java

--- src/main/java/eksamen_2023_2/Opgave6.java
+++ src/main/java/eksamen_2023_2/Opgave6.java
@@ -8,6 +8,39 @@ public class Opgave6 {
     //      o Cifrene er i faldende orden (eller lig med) fra venstre mod højre, fx 86421 og 97743.
     //Skriv en metode/algoritme, som kan afgøre om et tal tilhører klassen eller ej. Din løsning må gerne
     //indeholde flere metoder
-    
 
+    public static void main(String[] args) {
+        System.out.println(naturalOrderedNumbers(55321));
+    }
+
+    public static boolean naturalOrderedNumbers(int n) {
+        String numberString = String.valueOf(n);
+        //Determine if there are exactly 5 digits
+        if (numberString.length() != 5) {
+            return false;
+        }
+        return isAscending(numberString) || isDescending(numberString);
+    }
+
+    public static boolean isAscending(String n) {
+        char[] chars = n.toCharArray();
+
+        for (int i = 1; i < chars.length; i++) {
+            if (chars[i] < chars[i-1]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static boolean isDescending(String n) {
+        char[] chars = n.toCharArray();
+
+        for (int i = 1; i < chars.length; i++) {
+            if (chars[i] > chars[i-1]) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
