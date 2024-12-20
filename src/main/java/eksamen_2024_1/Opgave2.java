ec60cfd09ed82deac3019f11325014acc76e7a23
Andreas Witten Honoré <honore.andreas@gmail.com>
2024-12-20 21:37:55 +0100

    Update Opgave2.java

--- src/main/java/eksamen_2024_1/Opgave2.java
+++ src/main/java/eksamen_2024_1/Opgave2.java
@@ -7,5 +7,19 @@ public class Opgave2 {
     //tal deleligt med både tre og otte, tælles det kun med én gang.
     //Kaldt med parameteren 26 returneres 132 (3+6+8+9+12+15+16+18+21+24)
 
-    
+    public static void main(String[] args) {
+        System.out.println(sumDeleligMedTreOgOtte(26));
+    }
+
+    public static int sumDeleligMedTreOgOtte(int N) {
+        int sum = 0;
+        if (N <= 2) return 0;
+
+        if (N % 3 == 0 || N % 8 == 0) {
+            sum += N + sumDeleligMedTreOgOtte(N-1);
+        } else {
+            sum += sumDeleligMedTreOgOtte(N-1);
+        }
+        return sum;
+    }
 }
