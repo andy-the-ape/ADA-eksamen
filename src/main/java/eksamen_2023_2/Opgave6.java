01084631cfc5518471c7d19a006b1986fe722203
Andreas Witten Honoré <114078622+andy-the-ape@users.noreply.github.com>
2024-12-20 18:52:05 +0100

    Create Opgave6.java

---
+++ src/main/java/eksamen_2023_2/Opgave6.java
@@ -0,0 +1,13 @@
+package eksamen_2023_2;
+
+public class Opgave6 {
+    //Vi definerer en kategori af tal som har følgende egenskaber
+    //  • Naturlige tal på 5 cifre (10000-99999)
+    //  • Et af de to følgende kriterier skal være opfyldt
+    //      o Cifrene er i stigende orden (eller lig med) fra venstre mod højre, fx 36789 og 24469.
+    //      o Cifrene er i faldende orden (eller lig med) fra venstre mod højre, fx 86421 og 97743.
+    //Skriv en metode/algoritme, som kan afgøre om et tal tilhører klassen eller ej. Din løsning må gerne
+    //indeholde flere metoder
+    
+
+}
