bd9dbb95c8af1e47461cdbe8b1c08f2ec18ad12b
Andreas Witten Honoré <114078622+andy-the-ape@users.noreply.github.com>
2024-12-21 15:50:48 +0100

    Update Opgave1.java

--- src/main/java/eksamen_2024_2/Opgave1.java
+++ src/main/java/eksamen_2024_2/Opgave1.java
@@ -2,7 +2,7 @@ package eksamen_2024_2;
 
 public class Opgave1 {
 
-    public static boolean sumAfToTalLigParameter(int[] arr, int i, int x) {
+    public static boolean sumAfToTalLigParameter(int[] arr, int l, int x) {
         
     }
 }
