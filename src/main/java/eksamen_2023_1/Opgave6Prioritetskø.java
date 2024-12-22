2bf20d27eadc5f4ef44cf69a2b5fd7f92e07ecba
Andreas Witten Honoré <honore.andreas@gmail.com>
2024-12-22 11:47:58 +0100

    Update Opgave6Prioritetskø.java

--- src/main/java/eksamen_2023_1/Opgave6Prioritetskø.java
+++ src/main/java/eksamen_2023_1/Opgave6Prioritetskø.java
@@ -1,5 +1,15 @@
 package eksamen_2023_1;
 
+import java.util.Arrays;
+
 public class Opgave6Prioritetskø {
-    
+    public static void main(String[] args) {
+        System.out.println(getXelement(new int[]{13, 21, 16, 24, 31, 19, 68, 65, 26, 32}, 5));
+    }
+
+    public static int getXelement(int[] arr, int x) {
+        int[] heap = Arrays.copyOf(arr, arr.length);
+        Arrays.sort(heap);
+        return heap[x-1];
+    }
 }
